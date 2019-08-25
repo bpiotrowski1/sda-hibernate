d76a003a3232cd435ba7821ce04a243ddaf12804
bpiotrowski1 <baej.p26@gmail.com>
2019-08-25 09:40:30 +0200

    initial commit

--- src/main/java/sda/hibernate/entity/Uzytkownik.java
+++ src/main/java/sda/hibernate/entity/Uzytkownik.java
@@ -6,45 +6,45 @@ import javax.persistence.*;
 @Table(schema = "ksiegarnia", name = "uzytkownik")
 public class Uzytkownik {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column
-  private long id;
-  @Column
-  private String imie;
-  @Column
-  private String nazwisko;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private long id;
+    @Column
+    private String imie;
+    @Column
+    private String nazwisko;
 
-  public Uzytkownik() {
-  }
+    public Uzytkownik() {
+    }
 
-  public long getId() {
-    return id;
-  }
+    public long getId() {
+        return id;
+    }
 
-  public void setId(long id) {
-    this.id = id;
-  }
+    public void setId(long id) {
+        this.id = id;
+    }
 
-  public String getImie() {
-    return imie;
-  }
+    public String getImie() {
+        return imie;
+    }
 
-  public void setImie(String imie) {
-    this.imie = imie;
-  }
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
 
 
-  public String getNazwisko() {
-    return nazwisko;
-  }
+    public String getNazwisko() {
+        return nazwisko;
+    }
 
-  public void setNazwisko(String nazwisko) {
-    this.nazwisko = nazwisko;
-  }
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
 
-  public Uzytkownik(String imie, String nazwisko) {
-    this.imie = imie;
-    this.nazwisko = nazwisko;
-  }
+    public Uzytkownik(String imie, String nazwisko) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+    }
 }
